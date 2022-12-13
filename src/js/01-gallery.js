21770585f9b7e3f307f88d649b3ca182381f69d6
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-12-13 17:27:05 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const grid = document.querySelector('.gallery');
+
+galleryItems.forEach(img => {
+  grid.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`
+  );
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
