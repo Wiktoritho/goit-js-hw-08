179996fb3f72951b89ca23a4d0d079f1555a70ef
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-12-13 17:27:03 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const playerOn = new Player(iframe);
+let currentTime = 0;
+
+playerOn.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+console.log(playerOn)
+currentTime = localStorage.getItem('videoplayer-current-time')
+
+playerOn.setCurrentTime(currentTime);
