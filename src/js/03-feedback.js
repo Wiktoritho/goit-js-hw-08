39e53c473004d870a9ec7f9cd00d4807a51f09ad
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-12-13 17:27:02 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+let throttle = require('lodash.throttle');
 
+const input = document.querySelector("input");
+const message = document.querySelector("textarea");
+
+const object = {
+    input: input.value,
+    message: message.value,
+}
+
+input.addEventListener("input", () => {
+    localStorage.setItem('feedback-form-state', JSON.stringify(object));
+});
+
+console.log(localStorage)
